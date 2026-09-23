f10ebf27bb51910248821844bc72ad95c9bf6eee
Maxicosm <78530072+System32Booster@users.noreply.github.com>
2026-09-23 13:48:39 +0900

    Create script.js

---
+++ xmb/script.js
@@ -0,0 +1,6 @@
+const SFX_URLS = {
+  move: './sounds/move.wav',   // カーソル移動音
+  ok:   './sounds/ok.wav',     // 決定音
+  back: './sounds/back.wav',   // 戻る音
+  edge: './sounds/edge.wav'    // 端っこ音
+};
