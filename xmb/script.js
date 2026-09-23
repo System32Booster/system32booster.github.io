9744b38650344eb07e5054a708a55e45daf9bbe0
Maxicosm <78530072+System32Booster@users.noreply.github.com>
2026-09-23 14:16:06 +0900

    Delete xmb/script.js

--- xmb/script.js
+++
@@ -1,6 +0,0 @@
-const SFX_URLS = {
-  move: './sounds/snd_option.wav',   // カーソル移動音
-  ok:   './sounds/snd_option.wav',     // 決定音
-  back: './sounds/snd_cancel.wav',   // 戻る音
-  edge: './sounds/snd_option.wav'    // 端っこ音
-};
